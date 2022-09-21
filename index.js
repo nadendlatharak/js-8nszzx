a60c12a09c91b395e390d724c566c741e1c16cff
nadendlatharak <54901555+nadendlatharak@users.noreply.github.com>
2022-09-21 13:16:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h5>JS</h5>`;
+
+
+// let user = {
+//   name="john",
+//   age=23
+// }
+var id = Symbol("id");
+var user = {
+  age:23,
+  name:"Tharak",
+  [id]:1234,
+};
+
+console.log(user[id])
+console.log(user)
+
+// user[id] = "dasu";
+// console.log(user[id])
